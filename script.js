e584b04fd44940de62ecd2c85ad51c8c3375a2bc
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-07-05 08:24:11 +0600

    added text clear icon inside taskbox

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const clearIcon = document.getElementById("clear-icon"); //update
 
 function addTask() {
     if (inputBox.value === '') {
         alert("You must write something!");
-    }
-    else{
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        let sapn = document.createElement("span");
-        sapn.innerHTML = "\u00d7"
-        li.appendChild(sapn);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = "";
+    clearIcon.style.display = 'none'; //update
     saveData();
 }
 
-listContainer.addEventListener("click", function(e){
+listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
 }, false);
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
+function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+
+//update
+clearIcon.addEventListener("click", function () {
+    inputBox.value = '';
+    clearIcon.style.display = 'none';
+});
+
+inputBox.addEventListener("input", function () {
+    clearIcon.style.display = inputBox.value ? 'block' : 'none';
+});
